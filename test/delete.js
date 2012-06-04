66900aecc461684a88ef6bc3675e58e4c52abd06
ajlopez <webmaster@ajlopez.com>
2012-06-04 10:36:04 -0300

    Delete composed key, with test

--- test/delete.js
+++ test/delete.js
@@ -4,10 +4,19 @@ var simplestore = require('../'),
 
 var store = simplestore.createStore();
 
+// Simple Key
+
 store.put('user', 'adam');
 assert.equal(store.get('user'), 'adam');
 store.delete('user');
 assert.equal(store.get('user'), null);
+
+// Composed Key
+
+store.put('user:1:name', 'adam');
+assert.equal(store.get('user:1:name'), 'adam');
+store.delete('user:1:name');
+assert.equal(store.get('user:1:name'), null);
 
 
 
