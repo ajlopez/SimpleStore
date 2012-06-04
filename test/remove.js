f95d6c4f817a7b4d024a63f4257493cb0927bcfc
ajlopez <webmaster@ajlopez.com>
2012-06-04 18:33:39 -0300

    renamed delete to remove

--- test/remove.js
+++ test/remove.js
@@ -8,14 +8,14 @@ var store = simplestore.createStore();
 
 store.put('user', 'adam');
 assert.equal(store.get('user'), 'adam');
-store.delete('user');
+store.remove('user');
 assert.equal(store.get('user'), null);
 
 // Composed Key
 
 store.put('user:1:name', 'adam');
 assert.equal(store.get('user:1:name'), 'adam');
-store.delete('user:1:name');
+store.remove('user:1:name');
 assert.equal(store.get('user:1:name'), null);
 
 
