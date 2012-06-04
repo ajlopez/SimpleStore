e2e0aa7df3b01237b6f7cf7f4ce06051a59ca3d1
ajlopez <webmaster@ajlopez.com>
2012-06-04 10:38:50 -0300

    set null, as delete, with test

--- test/getSet.js
+++ test/getSet.js
@@ -4,9 +4,22 @@ var simplestore = require('../'),
 
 var store = simplestore.createStore();
 
+// Get undefined key
+
 assert.equal(store.get('foo'), null);
+
+// Simple put/get
+
 store.put('user', 'adam');
 assert.equal(store.get('user'), 'adam');
+
+// Put null
+
+store.put('user', null);
+assert.equal(store.get('user'), null);
+
+
+
 
 
 
